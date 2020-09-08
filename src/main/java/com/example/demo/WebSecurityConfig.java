e7610927985b8fca69c0d66e9fb0d734cd80a822
t-azuma-FS <t_azuma@f-support.co.jp>
2020-09-08 17:16:21 +0900

    JDBCによる user/authority の保存を実装

--- src/main/java/com/example/demo/WebSecurityConfig.java
+++ src/main/java/com/example/demo/WebSecurityConfig.java
@@ -1,21 +1,31 @@
 package com.example.demo;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 //import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.core.userdetails.User;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 //import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private DataSource datasource;
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
@@ -32,13 +42,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.logout()
 				.permitAll();
 	}
-    @Override
+
+	    @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-      PasswordEncoder encoder  =
-          PasswordEncoderFactories.createDelegatingPasswordEncoder();
-      auth.inMemoryAuthentication()
-          .withUser("user").password(encoder.encode("password")).roles("USER")
-          .and()
-          .withUser("admin").password(encoder.encode("adminpassword")).roles("USER","ADMIN");
-     }
+
+      auth
+          .jdbcAuthentication()
+          .dataSource(datasource)
+          .passwordEncoder(passwordEncoder())
+          .usersByUsernameQuery(
+        		  "SELECT username, password, enabled from users where username = ?")
+          .authoritiesByUsernameQuery(
+        		  "SELECT u.username, a.authority " +
+                  "FROM user_authorities a " +
+        		  "INNER JOIN users u ON u.username = a.username " +
+                  "WHERE u.username = ? ");
+      }
+
 }
